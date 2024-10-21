47bd54ffa11551d1c820d4c18461a348a8004b22
Jack Taylor <jackandrewtaylor1@gmail.com>
2024-10-21 09:54:14 +0000

    add function to determine the winner of the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,27 @@
-console.log('running');
+const choices = ["rock", "paper", "scissors", "lizard", "spock"];
+const rules = {
+    rock: ["scissors", "lizard"],
+    paper: ["rock", "spock"],
+    scissors: ["paper", "lizard"],
+    lizard: ["spock", "paper",],
+    spock: ["scissors","rock"]
+};
+// assigning the computer a random choice
+function getComputerChoice() {
+    return choices[Math.floor(Math.random() * choices.length)];
+};
+// function to track scoring
+let playerScore = 0;
+let computerScore = 0;
+// function to determine the winner
+function getWinner(playerChoice, computerChoice) {
+if (playerChoice === computerChoice) {
+    return "tie!"
+}
+else if (rules[playerChoice].includes(computerChoice)){
+return "player!"
+}
+else {
+    return "computer!"
+}
+};
