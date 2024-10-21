50d151c32321b1a3041ce541dea3fd52469f4a31
Jack Taylor <jackandrewtaylor1@gmail.com>
2024-10-21 08:54:08 +0000

    add random computer choice

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// list of choices and the rules of these choices
 const choices = [rock, paper, scissors, lizard, spock];
 const rules = {
     rock: ["scissors", "lizard"],
@@ -5,4 +6,8 @@ const rules = {
     scissors: ["paper", "lizard"],
     lizard: ["spock", "paper",],
     spock: ["scissors","rock"]
+}
+// assigning the computer a random choice
+function randomComputerChoice() {
+    return choices[Math.floor(Math.random() * choices.length)];
 }
