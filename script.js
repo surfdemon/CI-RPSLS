e32154563b9e6af18cfca17e3a42105624cce263
Jack Taylor <jackandrewtaylor1@gmail.com>
2024-10-21 08:42:10 +0000

    add choices and rules for game

---
+++ script.js
@@ -0,0 +1,8 @@
+const choices = [rock, paper, scissors, lizard, spock];
+const rules = {
+    rock: ["scissors", "lizard"],
+    paper: ["rock", "spock"],
+    scissors: ["paper", "lizard"],
+    lizard: ["spock", "paper",],
+    spock: ["scissors","rock"]
+}
