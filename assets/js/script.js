e7a0a6a4f73e6e61f5b5561c3dd807ea2976bb98
Robert Beach <surfdemon@gmail.com>
2024-10-22 09:41:08 +0000

    Maint: Move username js to main js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -186,3 +186,24 @@ document.querySelectorAll(".choiceButton").forEach(button => {
     });
 });
 
+const createNewUsername = () => {
+    const newUsername = document.querySelector("#newUsername").value;
+    const playerUsername = document.querySelector("#playerUsername");
+
+    if (newUsername.length > 15) {
+        alert("Please chose a shorter username...")
+    } else {
+        playerUsername.innerText = newUsername;
+        localStorage.username = newUsername;
+    }
+}
+
+document.getElementById("submitUsername").addEventListener("click", createNewUsername)
+
+document.getElementById("newUsername").addEventListener("keydown", function(event) {
+
+    if (event.key === "Enter") {
+        event.preventDefault();
+        createNewUsername();
+    }
+})
